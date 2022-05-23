70c6920fd78e33a66b07da85a58026c10008fcd8
shmuel <shmuel@shmuels-MacBook-Pro.local>
2022-05-23 20:03:51 +0300

    main table tweaks

--- UI/src/components/TableView.js
+++ UI/src/components/TableView.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import {observer} from "mobx-react"
 import ChainIcon from "./ChainIcon";
 import PlatformIcon from "./PlatformIcon";
+import LastUpdate from "./LastUpdate";
+import WhaleFriendly from "./WhaleFriendly";
 
 class TableView extends Component {
 
@@ -11,9 +13,8 @@ class TableView extends Component {
 
   render() {
     const data = this.props.data
-    debugger
-    const head = Object.values(data[0])
-    const body = data.slice(1)
+    const head = ['Name', 'Blockchains', 'TVL', 'Bad Debt (USD)', 'Last Update', 'Details']
+    const body = data
     return (
       <div>
         <table role="grid">
@@ -25,15 +26,24 @@ class TableView extends Component {
         <tbody>
           {body.map(row=> <tr>
             {Object.entries(row).map(([k, v])=> {
-              if (k === 'blockChains'){
-                return (<td><ChainIcon chain={v}/></td>)
-              }              
-              if (k === 'badDebt'){
-                return (<td>${v}</td>)
-              }              
-              if (k === 'name'){
+              if (k === 'platform'){
                 return (<td><PlatformIcon name={v}c/> {v}</td>)
               }
+              if (k === 'chain'){
+                return (<td><ChainIcon chain={v}/></td>)
+              }
+              if (k === 'tvl'){
+                return (<td><WhaleFriendly num={v}/></td>)
+              }                   
+              if (k === 'total'){
+                return (<td><WhaleFriendly num={v}/></td>)
+              }                  
+              if (k === 'updated'){
+                return (<td><LastUpdate timestamp={v}/></td>)
+              }               
+              if (k === 'users'){
+                return (<td>{v.length}</td>)
+              }            
             })}
           </tr>)}
         </tbody>
