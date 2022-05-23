70192729aefe35181057276f82849b77c28b3c23
shmuel <shmuel@shmuels-MacBook-Pro.local>
2022-05-23 20:03:15 +0300

    WhaleFriendly component

---
+++ UI/src/components/WhaleFriendly.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import {observer} from "mobx-react"
+
+class WhaleFriendly extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    let wfn
+    if(!this.props.num){
+      wfn = 'N/A'
+    }else if(this.props.num  <= 500) {
+      wfn = this.props.num
+    }else if(this.props.num / 1000 <= 500) {
+      wfn = (this.props.num / 1000).toFixed(2) + 'K'
+    } else if(this.props.num / 1000000 <= 500) {
+      wfn = (this.props.num / 1000000).toFixed(2) + 'M'
+    } else {
+      wfn = (this.props.num / 1000000000).toFixed(2) + 'B'
+    }
+    return (
+      <div>
+        {wfn}
+      </div>
+    )
+  }
+}
+
+export default observer(WhaleFriendly);
