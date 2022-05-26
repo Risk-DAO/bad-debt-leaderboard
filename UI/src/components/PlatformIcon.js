e2de6e4d4d4160336178faab7a5ce84dae137963
shmuel <shmuel@shmuels-MacBook-Pro.local>
2022-05-26 18:38:31 +0300

    cleanUp

--- UI/src/components/PlatformIcon.js
+++ UI/src/components/PlatformIcon.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import {observer} from "mobx-react"
 
-class ChainIcon extends Component {
+class PlatformIcon extends Component {
 
   constructor(props) {
     super(props);
   }
 
   render () {
-    const paltform = this.props.name.toLowerCase()
+    const platform = this.props.name.toLowerCase()
     return (
-      <img style={{borderRadius: '50%'}} src={`/images/platforms/${paltform}.webp`}/>
+      <img style={{borderRadius: '50%'}} src={`/images/platforms/${platform}.webp`}/>
     )
   }
 }
 
-export default ChainIcon;
+export default PlatformIcon;
