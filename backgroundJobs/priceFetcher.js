d530a605a2e9e4f1ec89940f8b3aa8986212c6b3
shmuel <shmuel@shmuels-MacBook-Pro.local>
2022-05-30 10:52:32 +0300

    priceFetcher spcial assets

--- backgroundJobs/priceFetcher.js
+++ backgroundJobs/priceFetcher.js
@@ -10,21 +10,41 @@ const coinGeckoChainIdMap = {
   BSC: 'binance-smart-chain',
 }
 
+const specialAssetPriceFetchers = {
+  AVAX_0x57319d41F71E81F3c65F2a47CA4e001EbAFd4F33: () => {
+    return 0
+  },
+  AVAX_0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7: () => {
+    return 0
+  },
+}
+
 const getPrice = async (network, address, web3) => {
   try {
     const { Contract } = web3.eth
     const token = new Contract(Addresses.erc20Abi, address)
     const decimal = await token.methods.decimals().call()
     const symbol = await token.methods.symbol().call()
-    let price
+    let apiPrice
     const coinGeckoApiCall = `https://api.coingecko.com/api/v3/simple/token_price/${coinGeckoChainIdMap[network]}?contract_addresses=${address}&vs_currencies=USD`
     console.log({coinGeckoApiCall})
-    const {data} = await axios.get(coinGeckoApiCall)
-    console.log(data)
-    const apiPrice = Object.values(data)[0].usd || 0
+    try{
+      const specialPriceFetcher = specialAssetPriceFetchers[`${network}_${address}`]
+      if(specialPriceFetcher){
+        apiPrice = specialPriceFetcher()
+      } else {
+        const {data} = await axios.get(coinGeckoApiCall)
+        //console.log(data)
+        apiPrice = Object.values(data)[0].usd || 0
+      }
+    } catch(e){
+      console.log('err: failed to fetch price for: ' + address)
+      console.error(e)
+      apiPrice = 0
+    }
     const normlizer = (18 - decimal).toString()
     console.log({ apiPrice })
-    price = toBN(toWei(apiPrice.toString())).mul(toBN('10').pow(toBN(normlizer)))
+    const price = toBN(toWei(apiPrice.toString())).mul(toBN('10').pow(toBN(normlizer)))
     console.log({
       address,
       symbol,
