ae2a1766de6efc6c4ebde0c51c451f664f00af10
shmuel <shmuel@shmuels-MacBook-Pro.local>
2022-06-21 12:04:39 +0300

    running subJobs

--- backgroundJobs/jobRuner.js
+++ backgroundJobs/jobRuner.js
@@ -7,6 +7,8 @@ const options = commandLineArgs([
   { name: 'name', alias: 'n', type: String },
   { name: 'index', alias: 'i', type: Number },
 ])
+const isSubJob = options.index === 0 || !!options.index
+
 const {uploadJsonFile} = require('../s3Client')
 const every5Minutes = 1000 * 60 * 5
 
@@ -23,7 +25,8 @@ function run() {
 
 function writeOutput (){
   if(comp && comp.output && comp.output.updated && comp.output.updated != lastUpdate){
-    uploadJsonFile(JSON.stringify(comp.output), options.name + '.json')
+    const s3FileName = isSubJob ? 'subjob' + options.name + '.json' : options.name + '.json'
+    uploadJsonFile(JSON.stringify(comp.output), s3FileName)
     lastUpdate = comp.output.updated
     console.log('output uploaded to S3')
   } else {
