93fe5d05e819668bad31125edac2783628efefb5
julien <julien@morpho.xyz>
2023-05-31 18:28:13 +0200

    setup ts minimal config

--- background.js
+++ background.js
@@ -5,127 +5,135 @@ const {sleep} = require('./utils');
 
 
 const jobs = [
+  // {
+  //   file: 'ParseAgave',
+  //   name: 'gnosis_agave'
+  // },
+  // {
+  //   file: 'ParseAave',
+  //   name: 'ethereum_aave'
+  // },
+  // {
+  //   file: 'ParseMaker',
+  //   name: 'ethereum_maker'
+  // },
+  // {
+  //   file: 'ParseCompound',
+  //   name: 'ethereum_compound'
+  // },
+  // {
+  //   file: 'ParseVenus',
+  //   name: 'BSC_venus'
+  // },
+  // {
+  //   file: 'ParseRari',
+  //   name: 'ethereum_rari-capital'
+  // },
+  // {
+  //   file: 'ParseOvix',
+  //   name: 'polygon_0vix'
+  // },
+  // {
+  //   file: 'ParseTraderJoe',
+  //   name: 'avalanche_trader-joe'
+  // },
+  // {
+  //   file: 'ParseIronBank',
+  //   name: 'ethereum_iron-bank'
+  // },
+  // {
+  //   file: 'ParseGranary',
+  //   name: 'FTM_granary'
+  // },
+  // {
+  //   file: 'ParseBenqi',
+  //   name: 'avalanche_benqi'
+  // },
+  // {
+  //   file: 'ParseBastion',
+  //   name: 'aurora_bastion'
+  // },
+  // {
+  //   file: 'ParseRikki',
+  //   name: 'BSC_rikki'
+  // },
+  // {
+  //   file: 'ParseApeswap',
+  //   name: 'BSC_apeswap'
+  // },
+  // {
+  //   file: 'ParseInverse',
+  //   name: 'ethereum_inverse'
+  // },
+  // {
+  //   file: 'ParseAurigami',
+  //   name: 'aurora_aurigami'
+  // },
+  // {
+  //   file: 'ParseTectonic',
+  //   name: 'CRO_tectonic'
+  // },
+  // {
+  //   file: 'ParseMim',
+  //   name: 'ethereum_MIM',
+  //   multiple: true
+  // },
+  // {
+  //   file: 'ParseMim_BSC',
+  //   name: 'BSC_MIM',
+  //   multiple: true
+  // },
+  // {
+  //   file: 'ParseMim_FTM',
+  //   name: 'FTM_MIM',
+  //   multiple: true
+  // },
+  // {
+  //   file: 'ParseMim_Arbitrum',
+  //   name: 'arbitrum_MIM',
+  //   multiple: true
+  // },
+  // {
+  //   file: 'ParseMim_AVAX',
+  //   name: 'avalanche_MIM',
+  //   multiple: true
+  // },
+  // {
+  //   file: 'ParseCream',
+  //   name: 'BSC_cream'
+  // },
+  // {
+  //   file: 'ParseMoonwell',
+  //   name: 'MOONBEAM_Moonwell'
+  // },
+  // {
+  //   file: 'ParseAaveV3_AVAX',
+  //   name: 'avalanche_aave v3'
+  // },
+  // {
+  //   file: 'ParseAaveV3_Arbitrum',
+  //   name: 'arbitrum_aave v3'
+  // },
+  // {
+  //   file: 'ParseAaveV3_Optimism',
+  //   name: 'optimism_aave v3'
+  // },
+  // {
+  //   file: 'ParseAaveV3_MATIC',
+  //   name: 'polygon_aave v3'
+  // },
+  // {
+  //   file: 'ParseSonne',
+  //   name: 'optimism_sonne'
+  // },
+  // {
+  //   file: "MorphoAaveV2Parser",
+  //   name: "Morpho AaveV2"
+  // },
   {
-    file: 'ParseAgave',
-    name: 'gnosis_agave'
-  },  
-  {
-    file: 'ParseAave',
-    name: 'ethereum_aave'
-  },
-  {
-    file: 'ParseMaker',
-    name: 'ethereum_maker'
-  },
-  {
-    file: 'ParseCompound',
-    name: 'ethereum_compound'
-  },
-  {
-    file: 'ParseVenus',
-    name: 'BSC_venus'
-  },  
-  {
-    file: 'ParseRari',
-    name: 'ethereum_rari-capital'
-  },  
-  {
-    file: 'ParseOvix',
-    name: 'polygon_0vix'
-  },  
-  {
-    file: 'ParseTraderJoe',
-    name: 'avalanche_trader-joe'
-  },  
-  {
-    file: 'ParseIronBank',
-    name: 'ethereum_iron-bank'
-  },  
-  {
-    file: 'ParseGranary',
-    name: 'FTM_granary'
-  },  
-  {
-    file: 'ParseBenqi',
-    name: 'avalanche_benqi'
-  },  
-  {
-    file: 'ParseBastion',
-    name: 'aurora_bastion'
-  },  
-  {
-    file: 'ParseRikki',
-    name: 'BSC_rikki'
-  },  
-  {
-    file: 'ParseApeswap',
-    name: 'BSC_apeswap'
-  },
-  {
-    file: 'ParseInverse',
-    name: 'ethereum_inverse'
-  },
-  {
-    file: 'ParseAurigami',
-    name: 'aurora_aurigami'
-  },
-  {
-    file: 'ParseTectonic',
-    name: 'CRO_tectonic'
-  },
-  {
-    file: 'ParseMim',
-    name: 'ethereum_MIM',
-    multiple: true
-  },  
-  {
-    file: 'ParseMim_BSC',
-    name: 'BSC_MIM',
-    multiple: true
-  },  
-  {
-    file: 'ParseMim_FTM',
-    name: 'FTM_MIM',
-    multiple: true
-  },  
-  {
-    file: 'ParseMim_Arbitrum',
-    name: 'arbitrum_MIM',
-    multiple: true
-  },  
-  {
-    file: 'ParseMim_AVAX',
-    name: 'avalanche_MIM',
-    multiple: true
-  },
-  {
-    file: 'ParseCream',
-    name: 'BSC_cream'
-  },
-  {
-    file: 'ParseMoonwell',
-    name: 'MOONBEAM_Moonwell'
-  },
-  {
-    file: 'ParseAaveV3_AVAX',
-    name: 'avalanche_aave v3'
-  },
-  {
-    file: 'ParseAaveV3_Arbitrum',
-    name: 'arbitrum_aave v3'
-  },
-  {
-    file: 'ParseAaveV3_Optimism',
-    name: 'optimism_aave v3'
-  },
-  { 
-    file: 'ParseAaveV3_MATIC',
-    name: 'polygon_aave v3'
-  },
-  {
-    file: 'ParseSonne',
-    name: 'optimism_sonne'
-  },
+    file: "MorphoCompoundParser",
+    name: "Morpho Compound"
+  }
   // { 
   //   file: 'ParseAaveV3_FTM',
   //   name: 'fantom_aavev3'
@@ -136,14 +144,14 @@ const jobs = [
   // }
 ]
 const runJob = (job, retry = 0) => {
-  const backgroundJob = fork('./backgroundJobs/jobRuner.js', [
+  const backgroundJob = fork('./dist/backgroundJobs/jobRuner.js', [
       '-f', job.file, 
       '-n', job.name,
       '-i', job.i,
     ],
     { silent: true }
   );
-  console.log(`--> job start "node ./backgroundJobs/jobRuner.js -f ${job.file} -n ${job.name} -i ${job.i}"`)
+  console.log(`--> job start "node ./dist/backgroundJobs/jobRuner.js -f ${job.file} -n ${job.name} -i ${job.i}"`)
   backgroundJob.stdout.on('data', (data) => {
     console.log(`${job.name} ${new Date().toLocaleString()} : ${data}`);
   });
@@ -154,10 +162,10 @@ const runJob = (job, retry = 0) => {
 
   backgroundJob.on('exit', code => {
     console.error(new Error(job.name + ' background job exited'))
-    console.log(`--X job died "node ./backgroundJobs/jobRuner.js -f ${job.file} -n ${job.name} -i ${job.i}"`)
+    console.log(`--X job died "node ./dist/backgroundJobs/jobRuner.js -f ${job.file} -n ${job.name} -i ${job.i}"`)
     if (retry < 100) { // preventing an infinite loop
       retry++
-      console.log(`--> job restart #${retry} "node ./backgroundJobs/jobRuner.js -f ${job.file} -n ${job.name} -i ${job.i} "`)
+      console.log(`--> job restart #${retry} "node ./dist/backgroundJobs/jobRuner.js -f ${job.file} -n ${job.name} -i ${job.i} "`)
       // recursion
       runJob(job, retry) // restarting the background job
     }
@@ -188,7 +196,7 @@ const init = async () => {
 
     await waitForCpuToGoBelowThreshold()
     if(job.multiple){    
-      let {subJobs} = require(`./backgroundJobs/${job.file}`)
+      let {subJobs} = require(`./dist/backgroundJobs/${job.file}`)
       console.log({'subJobs.length': subJobs.length})
       subJobs = subJobs.map((subJob, i) => Object.assign({}, job, { name: job.name + '_' + subJob.name, i }))
       subJobs.forEach(runJob);
